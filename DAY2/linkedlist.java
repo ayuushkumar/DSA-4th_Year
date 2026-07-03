ad153c6cc67a5f476537c7e0ffb482de465a9e86
Ayush <ayushkumar70505@gmail.com>
2026-07-03 12:45:03 +0530

    Added DAY2 linked list code

---
+++ DAY2/linkedlist.java
@@ -0,0 +1,66 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+    void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    // Display Linked List
+    void printList() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class linkedlist {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.addFirst(20);
+        list.addFirst(10);
+
+        list.addLast(30);
+        list.addLast(40);
+
+        list.printList();
+    }
+}
+
+//variable and datatype 
